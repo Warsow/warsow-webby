a9a2dac25f5e46b069bae4e88446dbdfbb555e18
Style Mistake <stylemistake@gmail.com>
2017-10-31 00:14:42 +0200

    Add a kitchen-sink

--- src/http/components/home.js
+++ src/http/components/home.js
@@ -1,10 +1,9 @@
 'use module';
 
-import { trim } from './util.js';
+import { compact, template } from './util.js';
 
 function layout(props = {}) {
-  const livereloadElem = '<script src="http://localhost:35729/livereload.js?snipver=1"></script>';
-  return trim(`
+  const page = template`
     <!doctype html>
     <html>
       <head>
@@ -16,25 +15,36 @@ function layout(props = {}) {
         <title>Warsow</title>
       </head>
       <body>
-        ${props.body || ''}
-        ${props.livereload ? livereloadElem : ''}
+        ${props.body}
+        ${props.livereload && `
+          <script src="http://localhost:35729/livereload.js?snipver=1"></script>
+        `}
       </body>
     </html>
-  `);
+  `;
+  return compact(page);
+}
+
+function button(props = {}) {
+  return template`
+    <div class="button-slanted" ${props.color && `style="background: ${props.color}"`}>
+      <div class="button-slanted-content">
+        ${props.text}
+      </div>
+    </div>
+  `;
 }
 
 export default (props = {}) => {
   return layout({
     ...props,
-    body: `
+    body: template`
       <div class="header">
         <div class="content-container">
           <img class="logo" src="/images/warsow.png">
-          <div class="button-slanted">
-            <div class="button-slanted-content">
-              Text
-            </div>
-          </div>
+          ${button({
+            text: 'Text',
+          })}
         </div>
       </div>
     `,
