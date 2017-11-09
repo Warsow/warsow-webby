e56f22530cb4a0d8563807555ac8d7e6c2dd1eda
Style Mistake <stylemistake@gmail.com>
2017-11-09 02:42:46 +0200

    Fix unplanned rapid DOM updates
    Function has lost its identity during minification, and it confused
    shit out of React. Replacing function with a class seems to fix this.

--- src/client/components/ServerList.jsx
+++ src/client/components/ServerList.jsx
@@ -12,7 +12,7 @@ export default class ServerList extends React.Component {
     super(props);
     // Create state
     this.state = {
-      servers: new Map(),
+      servers: {},
     };
     // Create livesow client
     const livesow = new Livesow();
@@ -38,100 +38,104 @@ export default class ServerList extends React.Component {
 }
 
 // Renders an individual server
-function Server(props) {
-  const server = props.server;
-  return (
-    <div className='server'>
-      <div className='server-header'>
-        <div className='server-name'>
-          <ColorEntities value={server.getName()} />
+class Server extends React.Component {
+
+  render() {
+    const server = this.props.server;
+    return (
+      <div className='server'>
+        <div className='server-header'>
+          <div className='server-name'>
+            <ColorEntities value={server.getName()} />
+          </div>
+          <div className='server-gametype'>
+            {server.getGameType()} / {server.getMapName()}
+          </div>
+          <div className='server-ip'>
+            {server.ip}:{server.port}
+          </div>
         </div>
-        <div className='server-gametype'>
-          {server.getGameType()} / {server.getMapName()}
-        </div>
-        <div className='server-ip'>
-          {server.ip}:{server.port}
-        </div>
+        {server.hasTeams() && (
+          <div className='server-team-alpha'>
+            <h3>
+              {server.getTeamAlphaName()}
+              <strong>{server.getTeamAlphaScore()}</strong>
+            </h3>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <th>Score</th>
+                  <th>Ping</th>
+                </tr>
+                {server.getTeamAlphaPlayers().map((x, i) => (
+                  <tr key={i}>
+                    <td><ColorEntities value={x.name} /></td>
+                    <td>{x.score}</td>
+                    <td>{x.ping}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {server.hasTeams() && (
+          <div className='server-team-beta'>
+            <h3>
+              {server.getTeamBetaName()}
+              <strong>{server.getTeamBetaScore()}</strong>
+            </h3>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <th>Score</th>
+                  <th>Ping</th>
+                </tr>
+                {server.getTeamBetaPlayers().map((x, i) => (
+                  <tr key={i}>
+                    <td><ColorEntities value={x.name} /></td>
+                    <td>{x.score}</td>
+                    <td>{x.ping}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {!server.hasTeams() && (
+          <div className='server-team-players'>
+            <h3>Players</h3>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <th>Score</th>
+                  <th>Ping</th>
+                </tr>
+                {server.getPlayers().map((x, i) => (
+                  <tr key={i}>
+                    <td><ColorEntities value={x.name} /></td>
+                    <td>{x.score}</td>
+                    <td>{x.ping}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {server.hasSpectators() && (
+          <div className='server-team-spec'>
+            <h3>Spectators</h3>
+            {server.getSpectators().map((x, i) => (
+              <div key={i} className='server-spec'>
+                <ColorEntities value={x.name} /> {x.ping}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-      {server.hasTeams() && (
-        <div className='server-team-alpha'>
-          <h3>
-            {server.getTeamAlphaName()}
-            <strong>{server.getTeamAlphaScore()}</strong>
-          </h3>
-          <table>
-            <tbody>
-              <tr>
-                <th>Name</th>
-                <th>Score</th>
-                <th>Ping</th>
-              </tr>
-              {server.getTeamAlphaPlayers().map((x, i) => (
-                <tr key={i}>
-                  <td><ColorEntities value={x.name} /></td>
-                  <td>{x.score}</td>
-                  <td>{x.ping}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-      {server.hasTeams() && (
-        <div className='server-team-beta'>
-          <h3>
-            {server.getTeamBetaName()}
-            <strong>{server.getTeamBetaScore()}</strong>
-          </h3>
-          <table>
-            <tbody>
-              <tr>
-                <th>Name</th>
-                <th>Score</th>
-                <th>Ping</th>
-              </tr>
-              {server.getTeamBetaPlayers().map((x, i) => (
-                <tr key={i}>
-                  <td><ColorEntities value={x.name} /></td>
-                  <td>{x.score}</td>
-                  <td>{x.ping}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-      {!server.hasTeams() && (
-        <div className='server-team-players'>
-          <h3>Players</h3>
-          <table>
-            <tbody>
-              <tr>
-                <th>Name</th>
-                <th>Score</th>
-                <th>Ping</th>
-              </tr>
-              {server.getPlayers().map((x, i) => (
-                <tr key={i}>
-                  <td><ColorEntities value={x.name} /></td>
-                  <td>{x.score}</td>
-                  <td>{x.ping}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-      {server.hasSpectators() && (
-        <div className='server-team-spec'>
-          <h3>Spectators</h3>
-          {server.getSpectators().map((x, i) => (
-            <div key={i} className='server-spec'>
-              <ColorEntities value={x.name} /> {x.ping}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+    );
+  }
+
 }
